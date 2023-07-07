ab30b7d71979730b5deb2615a15c143d370d18d0
Jory Roberts <robertsjory13@gmail.com>
2023-07-07 14:14:28 -0500

    completes lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -4,7 +4,6 @@ def return_evens(num_list):
     evens_list = ([num for num in num_list if num %2 == 0])
     return evens_list
 
-    pass
-
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = ([sentence + '!' for sentence in sentence_list])
+    return exclamation_list
